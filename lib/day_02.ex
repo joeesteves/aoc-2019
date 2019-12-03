e00f983fccb105ad9552f45b5ca78015ea80f687
Jose Esteves <jose@ceibo.co>
2019-12-02 23:07:41 -0300

    day 2 challenge B done

--- lib/day_02.ex
+++ lib/day_02.ex
@@ -1,10 +1,30 @@
 defmodule Day02 do
   def start do
-    process_data(
+    prepare_data(12, 2)
+    |> process_data
+  end
+
+  def prepare_data(noun, verb) do
+    [x, _, _ | rest] =
       Helper.load_data("data/day_02.txt")
       |> String.split(~r/(,|\n)/, trim: true)
       |> Enum.map(&String.to_integer/1)
-    )
+
+    [x, noun, verb | rest]
+  end
+
+  def get_noun_verb_for(magic_number) do
+    try do
+      for noun <- 0..99, verb <- 0..99 do
+        case prepare_data(noun, verb)
+             |> process_data do
+          ^magic_number -> throw({noun, verb})
+          _ -> nil
+        end
+      end
+    catch
+      x -> x
+    end
   end
 
   def process_data(data_input) do
@@ -25,11 +45,12 @@ defmodule Day02 do
         1 -> data[input1] + data[input2]
         2 -> data[input1] * data[input2]
       end
-      data = %{data | target => result}
 
-      data_input = List.update_at(data_input, target, fn _ -> result end)
+    data = %{data | target => result}
 
-      {_, rest_data} = data_input |> Enum.split(iteration * 4)
+    data_input = List.update_at(data_input, target, fn _ -> result end)
+
+    {_, rest_data} = data_input |> Enum.split(iteration * 4)
 
     process_chunk(rest_data, data_input, data, iteration + 1)
   end
