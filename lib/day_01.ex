b8e35023d53fba74b44a63ef3e180ce3876301e3
José Esteves <ajesteves@gmail.com>
2019-12-04 21:16:05 -0300

    stasj day 1

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -3,11 +3,24 @@ defmodule Day01 do
     Helper.load_data("data/day_01_A.txt")
     |> String.split
     |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&calculate_module_fuel/1)
+    |> Enum.map(&calculate_fuel/1)
     |> Enum.sum
   end
 
-  def calculate_module_fuel(mass) do
-    Float.floor(mass/3) - 2
+  def calculate_fuel(0, acc), do: acc
+
+  def calculate_fuel(mass, acc) do
+    fuel = calculate_fuel(mass)
+    calculate_fuel(fuel, acc + fuel)
   end
+
+
+  def calculate_fuel(mass) when mass > 0 do
+    case Float.floor(mass/3) - 2 do
+      x when x > 0 -> x
+      _ -> 0
+    end
+  end
+
+  def calculate_fuel(mass), do: 0
 end
--- test/day_01.ex
+++ test/day_01.ex
@@ -3,10 +3,16 @@ defmodule Day01Test do
   doctest Aoc
   import Day01
 
-  test "calculate_module_fuel" do
-    assert calculate_module_fuel(12) == 2
-    assert calculate_module_fuel(14) == 2
-    assert calculate_module_fuel(1969) == 654
-    assert calculate_module_fuel(100756) == 33583
+  test "calculate_fuel/1" do
+    assert calculate_fuel(12) == 2
+    assert calculate_fuel(14) == 2
+    assert calculate_fuel(1969) == 654
+    assert calculate_fuel(100756) == 33583
+  end
+
+  test "calculate_fuel/2" do
+    assert calculate_fuel(14, 0) == 2
+    assert calculate_fuel(1969, 0) == 966
+    assert calculate_fuel(100756, 0) == 50346
   end
 end
